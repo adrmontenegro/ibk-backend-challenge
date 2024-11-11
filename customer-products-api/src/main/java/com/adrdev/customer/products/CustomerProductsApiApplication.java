51c402c8d61c16504d8b9fa8bd6439873c917b3c
Adrian Montenegro <adrmontenegro97@gmail.com>
2024-11-11 04:35:46 -0500

    feat(customer-products-api): project init with spec

---
+++ customer-products-api/src/main/java/com/adrdev/customer/products/CustomerProductsApiApplication.java
@@ -0,0 +1,13 @@
+package com.adrdev.customer.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerProductsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerProductsApiApplication.class, args);
+	}
+
+}
